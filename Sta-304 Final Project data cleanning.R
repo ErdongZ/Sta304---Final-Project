1a37aa8f7325d2fbcdc3b2c5efa4fc90927a3515
ErdongZ <76109524+ErdongZ@users.noreply.github.com>
2020-12-20 22:30:28 +0800

    Add files via upload

---
+++ Sta-304 Final Project data cleanning.R
@@ -0,0 +1,47 @@
+library(haven)
+library(tidyverse)
+# Read in the raw data.
+setwd("E:/Assignments/STA304/Sta304-Final Project")
+raw_census_data <- read_dta("Sta304 - Final Project raw data.gz")
+
+
+# Add the labels
+raw_census_data <- labelled::to_factor(raw_census_data)
+
+reduced_census_data <- 
+  raw_census_data %>% 
+  select(age,
+         sex,
+         race,
+         educd,
+         stateicp,
+         classwkrd,
+         wkswork1,
+         inctot)
+
+reduced_census_data<-
+  reduced_census_data %>%
+  mutate(treatment = ifelse(sex=="female", 1, 0))
+
+reduced_census_data <- 
+  reduced_census_data %>%
+  filter(age != "less than 1 year old") %>%
+  filter(age != "90 (90+ in 1980 and 1990)")
+
+
+reduced_census_data <-
+  reduced_census_data %>%
+  filter(inctot != 9999999)%>%
+  filter(inctot > 0)
+
+
+reduced_census_data <-
+  reduced_census_data %>%
+  filter(classwkrd != "n/a")
+
+reduced_census_data <-
+  reduced_census_data %>%
+  filter(wkswork1 != 0)
+
+# Saving the census data as a csv file in my working directory
+write_csv(reduced_census_data, "E:/Assignments/STA304/Sta304-Final Project/Sta304 - Final Project cleaned data.csv")
